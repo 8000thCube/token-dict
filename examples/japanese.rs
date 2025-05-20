bf25a9f7ac36685946fb3beec747bdaaa954c199
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-05-19 23:04:20 -0700

    Add files via upload

---
+++ examples/japanese.rs
@@ -0,0 +1,17 @@
+pub fn main(){
+	let tokenizer:TokenDict=BufReader::new(File::open("日本語.txt").unwrap()).lines().filter_map(Result::ok).collect();
+	let tokens:Vec<u32>=tokenizer.tokenize_str("スペースは不要です").collect();
+	let detokens:String=tokenizer.detokenize_str(&tokens).collect();
+
+	print!("[");
+	for id in tokens.iter().take(tokens.len().saturating_sub(1)){print!("{id}, ")}
+	if let Some(id)=tokens.last(){print!("{id}")}
+	println!("]");
+	println!("\"{detokens}\"");
+}
+use {
+	std::{
+		fs::File,io::{BufRead,BufReader}
+	},
+	token_dict::{TokenDict}
+};
--- examples/token.rs
+++ examples/token.rs
@@ -1,15 +1,14 @@
 pub fn main(){
 	let tokenizer:TokenDict=BufReader::new(File::open("words.txt").unwrap()).lines().filter_map(Result::ok).collect();
-	let tokens:Vec<u32>=tokenizer.tokenize_str("text to tokenize here").collect();
+	let tokens:Vec<u32>=tokenizer.tokenize_str("some text to tokenize").collect();
 	let detokens:String=tokenizer.detokenize_str(&tokens).collect();
 
 	print!("[");
 	for id in tokens.iter().take(tokens.len().saturating_sub(1)){print!("{id}, ")}
 	if let Some(id)=tokens.last(){print!("{id}")}
 	println!("]");
-	println!("{detokens}");
+	println!("\"{detokens}\"");
 }
-
 use {
 	std::{
 		fs::File,io::{BufRead,BufReader}
