8f2b2b220eef64fdceb4b0fc6765038b0093d999
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-05-19 22:37:41 -0700

    Update token.rs

--- examples/token.rs
+++ examples/token.rs
@@ -1,22 +1,18 @@
 pub fn main(){
-	let tokenizer:TokenDict=["there","are","tokens","yay"].into_iter().collect();
-	let tokens:Vec<u32>=tokenizer.tokenize("there are tokens! yay".as_bytes().iter().map(|&b|b)).collect();
-	let detokens:Vec<u8>=tokenizer.detokenize(tokens.clone()).collect();
-	//dbg!(tokenizer);
-	dbg!(tokens);
-	dbg!(String::from_utf8_lossy(&detokens));
+	let tokenizer:TokenDict=BufReader::new(File::open("words.txt").unwrap()).lines().filter_map(Result::ok).collect();
+	let tokens:Vec<u32>=tokenizer.tokenize_str("text to tokenize here").collect();
+	let detokens:String=tokenizer.detokenize_str(&tokens).collect();
 
-	let iter=UTF8CharIter::from("correct string".as_bytes());
-	let c:String=iter.filter_map(|c|c.ok()).collect();
-	println!("{c}");
-
-	let iter=UTF8CharIter::from("incorrect string".bytes().chain([255,255]).chain(" incorrectness removed".bytes()));
-	let c:String=iter.filter_map(|c|c.ok()).collect();
-	println!("{c}");
-
-	let iter=UTF8CharIter::from("正しくない文字列".bytes().chain([128,255,0x20,255,0b10000000]).chain(" 不正確さは削除された".bytes()));
-	let c:String=iter.filter_map(|c|c.ok()).collect();
-	println!("{c}");
-
+	print!("[");
+	for id in tokens.iter().take(tokens.len().saturating_sub(1)){print!("{id}, ")}
+	if let Some(id)=tokens.last(){print!("{id}")}
+	println!("]");
+	println!("{detokens}");
 }
-use token_dict::{UTF8CharIter,TokenDict};
+
+use {
+	std::{
+		fs::File,io::{BufRead,BufReader}
+	},
+	token_dict::{TokenDict}
+};
